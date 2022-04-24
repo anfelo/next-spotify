9b3637c9eb7bc8c7cf8dddd24d38cabcc32c5713
Andres Osorio <af.osorio1341@gmail.com>
2022-04-24 12:05:22 -0500

    Added playlists page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,51 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/GradientLayout";
+import { useMe } from "../lib/hooks";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = ({ artists }) => {
+	const { user } = useMe();
 	return (
 		<GradientLayout
-			roundImage={false}
-			color="red"
+			roundImage
+			color="purple"
 			subtitle="profile"
-			title="Andres Osorio"
-			description="15 public playlists"
+			title={`${user?.firstName} ${user?.lastName}`}
+			description={`${user?.playlistsCount} public playlists`}
+			image="/portrait-100-pixel-1.png"
 		>
-			<div>hello</div>
+			<Box color="white" paddingX="40px">
+				<Box marginBottom="40px">
+					<Text fontSize="2xl" fontWeight="bold">
+						Top artist this month
+					</Text>
+					<Text fontSize="md">only visible to you</Text>
+				</Box>
+				<Flex>
+					{artists.map((artist) => (
+						<Box paddingX="10px" width="20%">
+							<Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+								<Image src="/portrait-100-pixel-1.png" borderRadius="100%" />
+								<Box marginTop="20px">
+									<Text fontSize="large">{artist.name}</Text>
+									<Text fontSize="x-small">Artist</Text>
+								</Box>
+							</Box>
+						</Box>
+					))}
+				</Flex>
+			</Box>
 		</GradientLayout>
 	);
+};
+
+export const getServerSideProps = async () => {
+	const artists = await prisma.artist.findMany({});
+
+	return {
+		props: { artists },
+	};
 };
 
 export default Home;
